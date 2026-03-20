29b491bf8e5cb06d5c3de76023d510a5f078f027
Peace Towett <peacetowett01@gmail.com>
2026-03-20 14:40:33 -0600

    fix: update fb-support link

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
       { source: "/wa-channel", destination: "https://www.whatsapp.com/channel/0029Vb4Zl7p3rZZivKYCL80M", permanent: false },
       { source: "/howto", destination: "https://www.scofhub.com/post/how-to-access-ss-topup-services", permanent: false },
       { source: "/wa", destination: "https://wa.me/254700042349?text=Hello", permanent: false },
+      { source: "/fb-support", destination: "https://www.facebook.com/peacevankiptoo.vankiptoo", permanent: false },
       { source: "/fb", destination: "https://www.facebook.com/kevin.rotich.9", permanent: false },
       { source: "/offline", destination: "https://www.scofhub.com/post/buy-ss-topup-packages-offline-using-paybill", permanent: false },
       { source: "/support", destination: "https://wa.me/254701087389?text=Hello", permanent: false },
